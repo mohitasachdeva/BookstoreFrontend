a1b3bc6beee9aba248f62cd4bb9d5b60948c7225
HP <sachdevamohita5@gmail.com>
2023-02-24 18:42:19 +0530

    header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CartComponent } from './component/cart/cart.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { OrderComponent } from './component/order/order.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatIconModule} from '@angular/material/icon'
+import{MatCardModule} from '@angular/material/card'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    DashboardComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
